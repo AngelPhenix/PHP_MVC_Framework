6c7d5ea33cab3301f24513c54da71ef4b3b55e78
AngelPhenix <breathstylebro@gmail.com>
2023-12-30 02:51:51 +0100

    Implemented getPath from the Router to clean up the URL and serve the right page

--- core/Request.php
+++ core/Request.php
@@ -5,7 +5,12 @@ namespace app\core;
 class Request 
 {
     public function getPath() {
-        // todo
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
+        $position = strpos($path, '?');
+        if($position === false) {
+            return $path;
+        }
+        return substr($path, 0, $position);
     }
 
     public function getMethod() {
--- core/Router.php
+++ core/Router.php
@@ -4,13 +4,19 @@ namespace app\core;
 
 class Router 
 {
+    public Request $request;
     protected array $routes = [];
+
+    public function __construct(\app\core\Request $request) {
+        $this->request = $request;
+    }
 
     public function get($path, $callback) {
         $this->routes['get'][$path] = $callback;
     }
 
     public function resolve() {
-        
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
     }
 }
