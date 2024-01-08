281686b8cd4ee9dda40347fead973f97f02c8a30
AngelPhenix <breathstylebro@gmail.com>
2024-01-08 18:32:56 +0100

    Added a main layout to output content from views

--- core/Router.php
+++ core/Router.php
@@ -7,15 +7,15 @@ class Router
     public Request $request;
     protected array $routes = [];
 
-    public function __construct(\app\core\Request $request) {
+    public function __construct(\app\core\Request $request){
         $this->request = $request;
     }
 
-    public function get($path, $callback) {
+    public function get($path, $callback){
         $this->routes['get'][$path] = $callback;
     }
 
-    public function resolve() {
+    public function resolve(){
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
@@ -28,7 +28,22 @@ class Router
         return call_user_func($callback);
     }
 
-    public function renderView($view) {
-        include_once __DIR__."/../views/$view.php";
+    public function renderView($view){
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+        include_once Application::$ROOT_DIR."/views/$view.php";
+    }
+
+    protected function layoutContent() {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view){
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 }
