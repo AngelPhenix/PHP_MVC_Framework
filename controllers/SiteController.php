1c5fb0b0c6d6cf29b1013e0ff1ec413c015ddff4
AngelPhenix <breathstylebro@gmail.com>
2024-01-09 01:33:29 +0100

    Finished implementing Controllers rendering params

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,7 +2,23 @@
 
 namespace app\controllers;
 
+use app\core\Application;
+
 class SiteController 
 {
-    
+    public function home(){
+        $params = [
+            'name' => "Jeremy"
+        ];
+        return Application::$app->router->renderView('home', $params);
+    }
+
+    public function contact(){
+
+        return Application::$app->router->renderView('contact');
+    }
+
+    public function handleContact(){
+        return 'Handling submitted data';
+    }
 }
