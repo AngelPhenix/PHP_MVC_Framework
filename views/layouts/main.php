75e075ff6cf01a2914c785ae90ae694f82187249
AngelPhenix <breathstylebro@gmail.com>
2024-01-09 02:02:33 +0100

    Added login and register in AuthController as well as views

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,10 +17,19 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link active" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                 <a class="nav-link" href="/contact">Contact</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                <a class="nav-link active" href="/login">Login</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/register">Register</a>
                 </li>
             </ul>
         </div>
