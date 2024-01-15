320b8f9dbf99db5b15eadde70bde31c8a04ded1a
AngelPhenix <breathstylebro@gmail.com>
2024-01-15 18:32:30 +0100

    populated register method from authController to load, validate and register data sent in post

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -14,14 +14,15 @@ class AuthController extends Controller
     }
 
     public function register(Request $request) {
+        $registerModel = new RegisterModel();
         if ($request->isPost()){
-            $registerModel = new RegisterModel();
-
-            
-
-            return 'Handle submitted data';
+            $registerModel->loadData($request->getBody());
+            if($registerModel->validate() && $registerModel->register()) {
+                return 'Success';
+            }
+            return $this->render('register', ['model' => $registerModel]);
         }
         $this->setLayout('auth');
-        return $this->render('register');
+        return $this->render('register', ['model' => $registerModel]);
     }
 }
