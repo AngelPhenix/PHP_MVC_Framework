768f6e6c15c0c6afbd72bf670e7f6ba564273824
AngelPhenix <breathstylebro@gmail.com>
2024-01-15 18:51:12 +0100

    Added validating const rules to parent Model

--- core/Model.php
+++ core/Model.php
@@ -4,6 +4,12 @@ namespace app\core;
 
 abstract class Model
 {
+    public const RULE_REQUIRED = 'required';
+    public const RULE_EMAIL = 'email';
+    public const RULE_MIN = 'min';
+    public const RULE_MAX = 'max';
+    public const RULE_MATCH = 'match';
+    
     public function loadData($data){
         foreach($data as $key => $value){
             if(property_exists($this, $key)) {
@@ -11,6 +17,8 @@ abstract class Model
             }
         }
     }
+
+    abstract public function rules(): array;
 
     public function validate(){
 
