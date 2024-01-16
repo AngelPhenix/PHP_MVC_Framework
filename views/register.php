6f7492c118b8fa1e0113915a1dc1408df6af6645
AngelPhenix <breathstylebro@gmail.com>
2024-01-16 03:59:32 +0100

    Added generic field to be used using a single line with different parameters

--- views/register.php
+++ views/register.php
@@ -1,27 +1,32 @@
 <h1>Register Page!</h1>
 
-<?php echo \app\core\form\Form::begin('', 'post') ?>
-  
+<?php $form = \app\core\form\Form::begin('', 'post') ?>
+  <?php echo $form->field($model, 'firstname') ?>
+  <?php echo $form->field($model, 'lastname') ?>
+  <?php echo $form->field($model, 'email') ?>
+  <?php echo $form->field($model, 'password') ?>
+  <?php echo $form->field($model, 'confirmPassword') ?>
+  <button type="submit" class="btn btn-primary">Submit</button>
 <?php \app\core\form\Form::end() ?>
 
 <form action="" method="post">
-  <div class=form-group">
+  <div class="form-group">
     <label>Firstname</label>
     <input type="text" name="firstname" class="form-control">
   </div>
-  <div class=form-group">
+  <div class="form-group">
     <label>Lastname</label>
     <input type="text" name="lastname" class="form-control">
   </div>
-  <div class=form-group">
+  <div class="form-group">
     <label>Email</label>
     <input type="email" name="email" class="form-control">
   </div>
-  <div class=form-group">
+  <div class="form-group">
     <label>Password</label>
     <input type="password" name="password" class="form-control">
   </div>
-  <div class=form-group">
+  <div class="form-group">
     <label>Confirm Password</label>
     <input type="password" name="confirmPassword" class="form-control">
   </div>
