d9f903a72376ca363c640f567d7c5e7450e2ebb9
AngelPhenix <breathstylebro@gmail.com>
2024-01-22 15:07:26 +0100

    Fixed bug where Database class doesn't know the namespace of the migrations and can't call their respective methods

--- core/Database.php
+++ core/Database.php
@@ -29,7 +29,8 @@ class Database
 
             require_once Application::$ROOT_DIR.'/migrations/'.$migration;
             $className = pathinfo($migration, PATHINFO_FILENAME);
-            $instance = new $className();
+            $class = "app\migrations\\".$className;
+            $instance = new $class();
             echo "Applying migration $migration".PHP_EOL;
             $instance->up();
             echo "Applied migration $migration".PHP_EOL;
