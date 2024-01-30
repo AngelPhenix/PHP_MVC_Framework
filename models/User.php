da00547ec799d7d32578cbee7e6224465b462e4f
AngelPhenix <breathstylebro@gmail.com>
2024-01-31 00:35:08 +0100

    Added user friendly text to forms

--- models/User.php
+++ models/User.php
@@ -41,4 +41,14 @@ class User extends DbModel
     public function attributes(): array{
         return ['firstname', 'lastname', 'email', 'password', 'status'];
     }
+
+    public function labels(): array{
+        return [
+            'firstname' => 'First name',
+            'lastname' => 'Last name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'confirmPassword' => 'Confirm password'
+        ]
+    }
 }
