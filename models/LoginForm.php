1551e293fd894703a8f05f410de3c15eb9cdee2d
AngelPhenix <breathstylebro@gmail.com>
2024-02-01 13:56:05 +0100

    Added redirect if login was successful

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -1,9 +1,14 @@
 <?php 
 
+namespace app\models;
+
 use app\core\Model;
 
 class LoginForm extends Model
 {
+    public string $email;
+    public string $password;
+
     public function rules(): array{
         return [
             'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
