bdd44d3584277196cc57e21c3e0ee074c2333183
AngelPhenix <breathstylebro@gmail.com>
2024-02-05 15:28:55 +0100

    Added BaseMiddleware

--- core/Controller.php
+++ core/Controller.php
@@ -2,9 +2,17 @@
 
 namespace app\core;
 
+use app\core\middlewares\BaseMiddleware;
+
 class Controller
 {
+
     public string $layout = 'main';
+
+    /**
+     * @var \app\core\middlewares\BaseMiddleware[]
+     */
+    public array $middlewares = [];
 
     public function setLayout($layout) {
         $this->layout = $layout;
@@ -12,5 +20,9 @@ class Controller
 
     public function render($view, $params = []){
         return Application::$app->router->renderView($view, $params);
+    }
+
+    public function registerMiddleware(BaseMiddleware $middleware){
+        $this->middlewares[] = $middleware;
     }
 }
