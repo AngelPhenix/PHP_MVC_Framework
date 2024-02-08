08f952d4efd1b69b7b16c9f1eea3abb21fa99af2
AngelPhenix <breathstylebro@gmail.com>
2024-02-09 00:12:57 +0100

    Fixed bug

--- core/form/InputField.php
+++ core/form/InputField.php
@@ -5,7 +5,7 @@ namespace app\core\form;
 use app\core\Model;
 use app\core\form\BaseField;
 
-class IntputField extends BaseField
+class InputField extends BaseField
 {
     public const TYPE_TEXT = 'text';
     public const TYPE_PASSWORD = 'password';
