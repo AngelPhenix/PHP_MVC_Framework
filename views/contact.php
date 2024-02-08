abee2a3728a2c21c4606e089f291c1e0e0407b2e
AngelPhenix <breathstylebro@gmail.com>
2024-02-08 14:14:18 +0100

    Implemented new textareaField class to contact page

--- views/contact.php
+++ views/contact.php
@@ -3,6 +3,9 @@
  * @var $this \app\core\View
  * @var $model \app\models\ContactForm
  */
+
+use app\core\form\TextareaField;
+
 $this->title = 'Contact';
 ?>
 
@@ -11,6 +14,6 @@ $this->title = 'Contact';
 <?php $form = \app\core\form\Form::begin('', 'post') ?>
 <?php echo $form->field($model, 'subject'); ?>
 <?php echo $form->field($model, 'email'); ?>
-<?php echo $form->field($model, 'body'); ?>
+<?php echo new TextareaField($model, 'body') ?>
 <button type="submit" class="btn btn-primary">Submit</button>
 <?php \app\core\form\Form::end();?>
